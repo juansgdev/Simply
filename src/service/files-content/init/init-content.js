4dc38efca6287779a5816ab1d7d987d2e039c3a3
juansgdev <juangomes.profissional@gmail.com>
2025-02-03 13:34:31 -0300

    init project feature complete

--- src/service/files-content/init/init-content.js
+++ src/service/files-content/init/init-content.js
@@ -1,16 +1,20 @@
-export const html = `<!DOCTYPE html>
+export const html = (projectName) => {
+    return `<!DOCTYPE html>
 <html>
 <head>
     <meta charset='utf-8'>
     <meta http-equiv='X-UA-Compatible' content='IE=edge'>
-    <title>Title</title>
+    <title>${projectName}</title>
     <meta name='viewport' content='width=device-width, initial-scale=1'>
+    <link rel='stylesheet' type='text/css' media='screen' href='global.css'>
 </head>
 
 <body>
     <h1>Simply Project!</h1>
+    
+    <script src='main.js'></script>
 </body>
-</html>`;
+</html>`};
 
 export const generatePackageJson = (projectName) => {
     return `{
--- src/service/init/init.js
+++ src/service/init/init.js
@@ -1,10 +1,10 @@
 import { mkdir, appendFile } from '../../modules/util.js';
-import { generatePackageJson, html } from './filesContent.js';
+import { generatePackageJson, html } from '../files-content/init/init-content.js';
 
 export const initSimplyProject = (projectName) => {
     mkdir(projectName);
 
-    appendFile(`${projectName}/index.html`, html);
+    appendFile(`${projectName}/index.html`, html(projectName));
     appendFile(`${projectName}/global.css`);
     appendFile(`${projectName}/main.js`);
     appendFile(`${projectName}/package.json`, generatePackageJson(projectName));
